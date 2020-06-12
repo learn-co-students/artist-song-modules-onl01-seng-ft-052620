0589e80b64170fcddff55b45f3d16afb52948072
francepants <franceline90@gmail.com>
2020-06-11 21:51:14 -0500

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,12 @@
+module Memorable
+
+        def reset_all
+            self.all.clear
+        end
+
+        def count
+            self.all.count
+        end
+end
+
+# Inside here, define your `reset_all` and `count` methods.
