7944b0f42eae883f14caac56f9946165b1b9d501
“Kyle <Kyledornan26@gmail.com>
2020-06-15 00:09:58 +0200

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Artist
+ include Paramable::Instance
+  extend Findable::FindMethod
   extend Memorable::ClassMethod
   attr_accessor :name
   attr_reader :songs
@@ -12,21 +14,13 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  #end
+  
 
   def add_song(song)
     @songs << song
@@ -37,7 +31,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,8 @@
+module Paramable 
+module Instance 
+    def to_param
+        name.downcase.gsub(' ', '-')
+      end
+end 
+
+end 
